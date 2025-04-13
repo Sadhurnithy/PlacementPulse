c6d422af2ae4fda7d325e6c8f370c07eac09ced2
Sadhur <111108668+Sadhurnithy@users.noreply.github.com>
2025-04-13 17:10:02 +0530

    Added EnableJpaRepositories Annotation

--- src/main/java/com/example/PlacementPulse/PlacementPulseApplication.java
+++ src/main/java/com/example/PlacementPulse/PlacementPulseApplication.java
@@ -2,8 +2,10 @@ package com.example.PlacementPulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.PlacementPulse.repository")
 public class PlacementPulseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlacementPulseApplication.class, args);
