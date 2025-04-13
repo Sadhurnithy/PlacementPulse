9deca982df2db1628bc0690ed3858cfa798bf6f6
Sadhur <111108668+Sadhurnithy@users.noreply.github.com>
2025-04-13 16:57:39 +0530

    @Autoweird Removed from Company Service

--- src/main/java/com/example/PlacementPulse/service/CompanyService.java
+++ src/main/java/com/example/PlacementPulse/service/CompanyService.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CompanyService {
     //Object Creation
-    @Autowired
     private final CompanyRepository companyRepoObj;
 
     //CREATE
